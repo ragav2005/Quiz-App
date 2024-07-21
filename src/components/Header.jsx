d9ebabf8284401bf993dc245d887b287fd643357
ragav vignes <74288437+ragav2005@users.noreply.github.com>
2024-07-21 22:23:58 +0530

    Update-Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="app-header">
-      <img src="/public/logo.svg" alt="React logo" />
+      <img src="https://raw.githubusercontent.com/ragav2005/Quiz-App/02926c551a172bb138ea06cc0190ab4f2a98a0a9/public/logo.svg" alt="React logo" />
       <h1>The React Quiz</h1>
     </header>
   );
